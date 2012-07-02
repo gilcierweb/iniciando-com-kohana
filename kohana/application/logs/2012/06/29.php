fef0d8c087bd93237f08e58773996e5a37144761
gilcierweb <gilcierweb@846f405d-fe61-700b-bed7-bc12fcc5fb76>
2012-07-02 20:50:39 +0000

    Inciando com Framewprk Kohana

---
+++ kohana/application/logs/2012/06/29.php
@@ -0,0 +1,70 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2012-06-29 09:12:15 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL kohana was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+2012-06-29 09:12:15 --- STRACE: HTTP_Exception_404 [ 404 ]: The requested URL kohana was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+--
+#0 D:\xampp\htdocs\kohana\system\classes\kohana\request\client.php(64): Kohana_Request_Client_Internal->execute_request(Object(Request))
+#1 D:\xampp\htdocs\kohana\system\classes\kohana\request.php(1138): Kohana_Request_Client->execute(Object(Request))
+#2 D:\xampp\htdocs\kohana\index.php(109): Kohana_Request->execute()
+#3 {main}
+2012-06-29 09:13:25 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL kohana was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+2012-06-29 09:13:25 --- STRACE: HTTP_Exception_404 [ 404 ]: The requested URL kohana was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+--
+#0 D:\xampp\htdocs\kohana\system\classes\kohana\request\client.php(64): Kohana_Request_Client_Internal->execute_request(Object(Request))
+#1 D:\xampp\htdocs\kohana\system\classes\kohana\request.php(1138): Kohana_Request_Client->execute(Object(Request))
+#2 D:\xampp\htdocs\kohana\index.php(109): Kohana_Request->execute()
+#3 {main}
+2012-06-29 09:13:33 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL kohana was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+2012-06-29 09:13:33 --- STRACE: HTTP_Exception_404 [ 404 ]: The requested URL kohana was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+--
+#0 D:\xampp\htdocs\kohana\system\classes\kohana\request\client.php(64): Kohana_Request_Client_Internal->execute_request(Object(Request))
+#1 D:\xampp\htdocs\kohana\system\classes\kohana\request.php(1138): Kohana_Request_Client->execute(Object(Request))
+#2 D:\xampp\htdocs\kohana\index.php(109): Kohana_Request->execute()
+#3 {main}
+2012-06-29 11:18:06 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL kohana was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+2012-06-29 11:18:06 --- STRACE: HTTP_Exception_404 [ 404 ]: The requested URL kohana was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+--
+#0 D:\xampp\htdocs\kohana\system\classes\kohana\request\client.php(64): Kohana_Request_Client_Internal->execute_request(Object(Request))
+#1 D:\xampp\htdocs\kohana\system\classes\kohana\request.php(1138): Kohana_Request_Client->execute(Object(Request))
+#2 D:\xampp\htdocs\kohana\index.php(109): Kohana_Request->execute()
+#3 {main}
+2012-06-29 11:18:11 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL kohana was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+2012-06-29 11:18:11 --- STRACE: HTTP_Exception_404 [ 404 ]: The requested URL kohana was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+--
+#0 D:\xampp\htdocs\kohana\system\classes\kohana\request\client.php(64): Kohana_Request_Client_Internal->execute_request(Object(Request))
+#1 D:\xampp\htdocs\kohana\system\classes\kohana\request.php(1138): Kohana_Request_Client->execute(Object(Request))
+#2 D:\xampp\htdocs\kohana\index.php(109): Kohana_Request->execute()
+#3 {main}
+2012-06-29 11:18:21 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: kohana/index.php ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2012-06-29 11:18:21 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: kohana/index.php ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 D:\xampp\htdocs\kohana\index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-06-29 11:22:25 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL kohana was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+2012-06-29 11:22:25 --- STRACE: HTTP_Exception_404 [ 404 ]: The requested URL kohana was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+--
+#0 D:\xampp\htdocs\kohana\system\classes\kohana\request\client.php(64): Kohana_Request_Client_Internal->execute_request(Object(Request))
+#1 D:\xampp\htdocs\kohana\system\classes\kohana\request.php(1138): Kohana_Request_Client->execute(Object(Request))
+#2 D:\xampp\htdocs\kohana\index.php(109): Kohana_Request->execute()
+#3 {main}
+2012-06-29 12:25:24 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL gil was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+2012-06-29 12:25:24 --- STRACE: HTTP_Exception_404 [ 404 ]: The requested URL gil was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+--
+#0 D:\xampp\htdocs\kohana\system\classes\kohana\request\client.php(64): Kohana_Request_Client_Internal->execute_request(Object(Request))
+#1 D:\xampp\htdocs\kohana\system\classes\kohana\request.php(1138): Kohana_Request_Client->execute(Object(Request))
+#2 D:\xampp\htdocs\kohana\index.php(109): Kohana_Request->execute()
+#3 {main}
+2012-06-29 13:07:23 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL gil was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+2012-06-29 13:07:23 --- STRACE: HTTP_Exception_404 [ 404 ]: The requested URL gil was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+--
+#0 D:\xampp\htdocs\kohana\system\classes\kohana\request\client.php(64): Kohana_Request_Client_Internal->execute_request(Object(Request))
+#1 D:\xampp\htdocs\kohana\system\classes\kohana\request.php(1138): Kohana_Request_Client->execute(Object(Request))
+#2 D:\xampp\htdocs\kohana\index.php(109): Kohana_Request->execute()
+#3 {main}
+2012-06-29 13:07:27 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL gil was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+2012-06-29 13:07:27 --- STRACE: HTTP_Exception_404 [ 404 ]: The requested URL gil was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+--
+#0 D:\xampp\htdocs\kohana\system\classes\kohana\request\client.php(64): Kohana_Request_Client_Internal->execute_request(Object(Request))
+#1 D:\xampp\htdocs\kohana\system\classes\kohana\request.php(1138): Kohana_Request_Client->execute(Object(Request))
+#2 D:\xampp\htdocs\kohana\index.php(109): Kohana_Request->execute()
+#3 {main}
---
+++ kohana/application/views/site/index.php
@@ -0,0 +1,51 @@
+<h1>Home</h1>
+<div>TODO write content</div>
+
+<a class="btn" href="">Link</a>
+<button class="btn" type="submit">   Button</button>
+<input class="btn btn-danger" type="button"   value="Input">
+<input class="btn" type="submit"   value="Submit">
+
+<div class="btn-group">
+    <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
+        Action
+        <span class="caret"></span>
+    </a>
+    <ul class="dropdown-menu">
+        <!-- dropdown menu links -->
+    </ul>
+</div>
+
+<ul class="nav nav-pills">
+    <li class="dropdown">
+        <a class="dropdown-toggle"
+           data-toggle="dropdown"
+           href="#">
+            Dropdown
+            <b class="caret"></b>
+        </a>
+        <ul class="dropdown-menu">
+            <!-- links -->
+        </ul>
+    </li>
+</ul>
+
+<!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+    <script src="<?php echo url::base();?>bootstrap/js/jquery.js"></script>
+    <script src="<?php echo url::base();?>bootstrap/js/google-code-prettify/prettify.js"></script>
+    <script src="<?php echo url::base();?>bootstrap/js/bootstrap-transition.js"></script>
+    <script src="<?php echo url::base();?>bootstrap/js/bootstrap-alert.js"></script>
+    <script src="<?php echo url::base();?>bootstrap/js/bootstrap-modal.js"></script>
+    <script src="<?php echo url::base();?>bootstrap/js/bootstrap-dropdown.js"></script>
+    <script src="<?php echo url::base();?>bootstrap/js/bootstrap-scrollspy.js"></script>
+    <script src="<?php echo url::base();?>bootstrap/js/bootstrap-tab.js"></script>
+    <script src="<?php echo url::base();?>bootstrap/js/bootstrap-tooltip.js"></script>
+    <script src="<?php echo url::base();?>bootstrap/js/bootstrap-popover.js"></script>
+    <script src="<?php echo url::base();?>bootstrap/js/bootstrap-button.js"></script>
+    <script src="<?php echo url::base();?>bootstrap/js/bootstrap-collapse.js"></script>
+    <script src="<?php echo url::base();?>bootstrap/js/bootstrap-carousel.js"></script>
+    <script src="<?php echo url::base();?>bootstrap/js/bootstrap-typeahead.js"></script>
+    <script src="<?php echo url::base();?>bootstrap/js/application.js"></script>

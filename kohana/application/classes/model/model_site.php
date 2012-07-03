cd27b23b862e9bdd3906e5bec4067a33ff491ea0
gilcierweb <gilcierweb@846f405d-fe61-700b-bed7-bc12fcc5fb76>
2012-07-03 14:41:13 +0000

    primeiro

---
+++ kohana/application/classes/model/model_site.php
@@ -0,0 +1,15 @@
+<?php
+class Model_Site extends Model_Database
+{
+    public function form_lista()
+    {
+        // Get stuff from the database:
+        //return $this->db->query(...);
+    }
+    public function form_insert()
+    {
+        // This is where you do domain logic...
+    }
+ 
+   
+}

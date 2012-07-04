9631e817de3dca9f2f45579a2490d4edbba02c80
gilcierweb <gilcierweb@846f405d-fe61-700b-bed7-bc12fcc5fb76>
2012-07-04 13:25:50 +0000

    alterações

--- kohana/application/views/site/list.php
+++ kohana/application/views/site/list.php
@@ -1,19 +1,17 @@
-<code>
-    <h1>Lista de Dados ( <?php echo html::anchor('blog/form', 'Adicionar novo'); ?> )</h1>
-    <?php foreach ($dados as $dado) { ?>    
+<h1>Lista de Dados ( <?php echo html::anchor('blog/form', 'Adicionar novo'); ?> )</h1>
+<?php foreach ($dados as $dado) { ?>    
 
-        <h2><?php echo $dado->dado_titulo; ?></h2>
-        <p>
-            <?php echo $dado->dado_resumo; ?>
-        </p>
-        <p>
-            <?php echo $dado->dado_descricao; ?>
-        </p>
-        <span>
-            <?php echo html::anchor('blog/form/' . $dado->dado_id, '[ Editar ] '); ?> 
-            <?php echo html::anchor('blog/delete/' . $dado->dado_id, '[ Excluir ] '); ?> 
-        </span>
-        <hr />    
+    <h2><?php echo $dado->dado_titulo; ?></h2>
+    <p>
+        <?php echo $dado->dado_resumo; ?>
+    </p>
+    <p>
+        <?php echo $dado->dado_descricao; ?>
+    </p>
+    <span>
+        <?php echo html::anchor('blog/form/' . $dado->dado_id, '[ Editar ] '); ?> 
+        <?php echo html::anchor('blog/delete/' . $dado->dado_id, '[ Excluir ] '); ?> 
+    </span>
+    <hr />    
 
-    <?php } ?>
-</code>
+<?php } ?>

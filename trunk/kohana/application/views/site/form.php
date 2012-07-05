a61eb5ed6f05fafba324e8a90c680724aa6899b4
gilcierweb <gilcierweb@846f405d-fe61-700b-bed7-bc12fcc5fb76>
2012-07-05 18:57:00 +0000

    alterações

--- trunk/kohana/application/views/site/form.php
+++ trunk/kohana/application/views/site/form.php
@@ -1,5 +1,5 @@
 <?php echo form::open('site/form_insert', array('class' => 'well form-horizontal')); ?>
-<input type="text" name="dado_titulo" class="input-xsmall"/>
-<input type="text" name="dado_resumo" class="input-xsmall"/>
-<textarea name="dado_descricao" cols="50" rows="10" class="input-xlarge"></textarea>
+  <?php echo Form::input("dado_titulo", $article->dado_titulo,array('class' => 'input-xsmall')); ?>
+  <?php echo Form::input("dado_resumo", $article->dado_resumo,array('class' => 'input-xsmall')); ?>
+  <?php echo Form::textarea("dado_descricao", $article->dado_descricao,array('class' => 'input-xlarge')); ?>
 <?php echo form::submit('submit', 'Send', array('class' => 'btn')); ?>

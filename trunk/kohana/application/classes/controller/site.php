18e350ed99ac19c93c1ebf91fa04cfba5ef4f08b
gilcierweb <gilcierweb@846f405d-fe61-700b-bed7-bc12fcc5fb76>
2012-07-05 17:50:21 +0000

    alterações

--- trunk/kohana/application/classes/controller/site.php
+++ trunk/kohana/application/classes/controller/site.php
@@ -6,7 +6,7 @@ class Controller_Site extends Controller_Template
 {
 
     public $template = 'template'; // bug 02/07/12 só aceita o nome $template porque herda da class Controller_Template
-
+              
     public function action_index()
     {
         $this->template->content = View::factory('site/index');

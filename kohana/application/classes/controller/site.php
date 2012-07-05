dc05eafeac5073eb1157c60c6fe399c30095afc6
gilcierweb <gilcierweb@846f405d-fe61-700b-bed7-bc12fcc5fb76>
2012-07-05 18:57:00 +0000

    alterações

--- kohana/application/classes/controller/site.php
+++ kohana/application/classes/controller/site.php
@@ -6,7 +6,7 @@ class Controller_Site extends Controller_Template
 {
 
     public $template = 'template'; // bug 02/07/12 só aceita o nome $template porque herda da class Controller_Template
-              
+
     public function action_index()
     {
         $this->template->content = View::factory('site/index');
@@ -31,7 +31,35 @@ class Controller_Site extends Controller_Template
 
     public function action_form()
     {
+        $article_id = $this->request->param('id');
+        $article = new Model_Site($article_id);
+        $article->values($_POST); // populate $article object from $_POST array
+        $article->save(); // saves article to database
+
         $this->template->content = View::factory('site/form');
+    }
+
+    // loads the new article form
+    public function action_new()
+    {
+        $article = new Model_Article();
+
+        $view = new View('article/edit');
+        $view->set("article", $article);
+
+        $this->response->body($view);
+    }
+
+    // edit the article
+    public function action_edit()
+    {
+        $article_id = $this->request->param('id');
+        $article = new Model_Article($article_id);
+
+        $view = new View('article/edit');
+        $view->set("article", $article);
+
+        $this->response->body($view);
     }
 
     public function action_form_insert()
@@ -47,8 +75,19 @@ class Controller_Site extends Controller_Template
         }
     }
 
+// save the article
+    public function action_post()
+    {
+        $article_id = $this->request->param('id');
+        $article = new Model_Site($article_id);
+        $article->values($_POST); // populate $article object from $_POST array
+        $article->save(); // saves article to database         
+        $this->request->redirect('site/list');
+    }
+
     public function action_deletar()
-    {$this->session = Session::instance();
+    {
+        $this->session = Session::instance();
         $id = $this->request->param('id'); //= $this->uri->segment(3); // igual ao codeigniter
         //echo $this->request->param('id');
         //Apaga o objeto post com o id passado
@@ -56,12 +95,12 @@ class Controller_Site extends Controller_Template
         $deletar = ORM::factory('site', $id);
         // Use o ORM::loaded  para verificar se ORM carregado com êxito um registro..
         if ($deletar->loaded()) {
-            $deletar->delete();           
+            $deletar->delete();
             $this->session->set('msg', '<div class="alert alert-success">
 <a class="close" data-dismiss="alert" href="#">×</a><h1>Registro apagado com sucesso!!</h1></div>');
         } else {
             echo 'Error: Não carregou o model!!';
-        }        
+        }
 //        ORM::factory('site',$id)
 //                ->delete();
         //tá dando erro model não carregado não sei resolver
--- kohana/application/logs/2012/07/05.php
+++ kohana/application/logs/2012/07/05.php
@@ -253,3 +253,122 @@
 --
 #0 [internal function]: Kohana_Core::shutdown_handler()
 #1 {main}
+2012-07-05 15:37:50 --- ERROR: ErrorException [ 8 ]: Undefined variable: article ~ APPPATH\views\site\form.php [ 2 ]
+2012-07-05 15:37:50 --- STRACE: ErrorException [ 8 ]: Undefined variable: article ~ APPPATH\views\site\form.php [ 2 ]
+--
+#0 D:\xampp\htdocs\kohana\application\views\site\form.php(2): Kohana_Core::error_handler(8, 'Undefined varia...', 'D:\xampp\htdocs...', 2, Array)
+#1 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(61): include('D:\xampp\htdocs...')
+#2 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(343): Kohana_View::capture('D:\xampp\htdocs...', Array)
+#3 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(228): Kohana_View->render()
+#4 D:\xampp\htdocs\kohana\application\views\template.php(22): Kohana_View->__toString()
+#5 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(61): include('D:\xampp\htdocs...')
+#6 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(343): Kohana_View::capture('D:\xampp\htdocs...', Array)
+#7 D:\xampp\htdocs\kohana\system\classes\kohana\controller\template.php(44): Kohana_View->render()
+#8 [internal function]: Kohana_Controller_Template->after()
+#9 D:\xampp\htdocs\kohana\system\classes\kohana\request\client\internal.php(121): ReflectionMethod->invoke(Object(Controller_Site))
+#10 D:\xampp\htdocs\kohana\system\classes\kohana\request\client.php(64): Kohana_Request_Client_Internal->execute_request(Object(Request))
+#11 D:\xampp\htdocs\kohana\system\classes\kohana\request.php(1138): Kohana_Request_Client->execute(Object(Request))
+#12 D:\xampp\htdocs\kohana\index.php(109): Kohana_Request->execute()
+#13 {main}
+2012-07-05 15:39:58 --- ERROR: ErrorException [ 8 ]: Undefined variable: article ~ APPPATH\views\site\form.php [ 2 ]
+2012-07-05 15:39:58 --- STRACE: ErrorException [ 8 ]: Undefined variable: article ~ APPPATH\views\site\form.php [ 2 ]
+--
+#0 D:\xampp\htdocs\kohana\application\views\site\form.php(2): Kohana_Core::error_handler(8, 'Undefined varia...', 'D:\xampp\htdocs...', 2, Array)
+#1 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(61): include('D:\xampp\htdocs...')
+#2 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(343): Kohana_View::capture('D:\xampp\htdocs...', Array)
+#3 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(228): Kohana_View->render()
+#4 D:\xampp\htdocs\kohana\application\views\template.php(22): Kohana_View->__toString()
+#5 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(61): include('D:\xampp\htdocs...')
+#6 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(343): Kohana_View::capture('D:\xampp\htdocs...', Array)
+#7 D:\xampp\htdocs\kohana\system\classes\kohana\controller\template.php(44): Kohana_View->render()
+#8 [internal function]: Kohana_Controller_Template->after()
+#9 D:\xampp\htdocs\kohana\system\classes\kohana\request\client\internal.php(121): ReflectionMethod->invoke(Object(Controller_Site))
+#10 D:\xampp\htdocs\kohana\system\classes\kohana\request\client.php(64): Kohana_Request_Client_Internal->execute_request(Object(Request))
+#11 D:\xampp\htdocs\kohana\system\classes\kohana\request.php(1138): Kohana_Request_Client->execute(Object(Request))
+#12 D:\xampp\htdocs\kohana\index.php(109): Kohana_Request->execute()
+#13 {main}
+2012-07-05 15:39:59 --- ERROR: ErrorException [ 8 ]: Undefined variable: article ~ APPPATH\views\site\form.php [ 2 ]
+2012-07-05 15:39:59 --- STRACE: ErrorException [ 8 ]: Undefined variable: article ~ APPPATH\views\site\form.php [ 2 ]
+--
+#0 D:\xampp\htdocs\kohana\application\views\site\form.php(2): Kohana_Core::error_handler(8, 'Undefined varia...', 'D:\xampp\htdocs...', 2, Array)
+#1 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(61): include('D:\xampp\htdocs...')
+#2 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(343): Kohana_View::capture('D:\xampp\htdocs...', Array)
+#3 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(228): Kohana_View->render()
+#4 D:\xampp\htdocs\kohana\application\views\template.php(22): Kohana_View->__toString()
+#5 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(61): include('D:\xampp\htdocs...')
+#6 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(343): Kohana_View::capture('D:\xampp\htdocs...', Array)
+#7 D:\xampp\htdocs\kohana\system\classes\kohana\controller\template.php(44): Kohana_View->render()
+#8 [internal function]: Kohana_Controller_Template->after()
+#9 D:\xampp\htdocs\kohana\system\classes\kohana\request\client\internal.php(121): ReflectionMethod->invoke(Object(Controller_Site))
+#10 D:\xampp\htdocs\kohana\system\classes\kohana\request\client.php(64): Kohana_Request_Client_Internal->execute_request(Object(Request))
+#11 D:\xampp\htdocs\kohana\system\classes\kohana\request.php(1138): Kohana_Request_Client->execute(Object(Request))
+#12 D:\xampp\htdocs\kohana\index.php(109): Kohana_Request->execute()
+#13 {main}
+2012-07-05 15:40:00 --- ERROR: ErrorException [ 8 ]: Undefined variable: article ~ APPPATH\views\site\form.php [ 2 ]
+2012-07-05 15:40:00 --- STRACE: ErrorException [ 8 ]: Undefined variable: article ~ APPPATH\views\site\form.php [ 2 ]
+--
+#0 D:\xampp\htdocs\kohana\application\views\site\form.php(2): Kohana_Core::error_handler(8, 'Undefined varia...', 'D:\xampp\htdocs...', 2, Array)
+#1 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(61): include('D:\xampp\htdocs...')
+#2 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(343): Kohana_View::capture('D:\xampp\htdocs...', Array)
+#3 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(228): Kohana_View->render()
+#4 D:\xampp\htdocs\kohana\application\views\template.php(22): Kohana_View->__toString()
+#5 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(61): include('D:\xampp\htdocs...')
+#6 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(343): Kohana_View::capture('D:\xampp\htdocs...', Array)
+#7 D:\xampp\htdocs\kohana\system\classes\kohana\controller\template.php(44): Kohana_View->render()
+#8 [internal function]: Kohana_Controller_Template->after()
+#9 D:\xampp\htdocs\kohana\system\classes\kohana\request\client\internal.php(121): ReflectionMethod->invoke(Object(Controller_Site))
+#10 D:\xampp\htdocs\kohana\system\classes\kohana\request\client.php(64): Kohana_Request_Client_Internal->execute_request(Object(Request))
+#11 D:\xampp\htdocs\kohana\system\classes\kohana\request.php(1138): Kohana_Request_Client->execute(Object(Request))
+#12 D:\xampp\htdocs\kohana\index.php(109): Kohana_Request->execute()
+#13 {main}
+2012-07-05 15:40:57 --- ERROR: ErrorException [ 8 ]: Undefined variable: article ~ APPPATH\views\site\form.php [ 2 ]
+2012-07-05 15:40:57 --- STRACE: ErrorException [ 8 ]: Undefined variable: article ~ APPPATH\views\site\form.php [ 2 ]
+--
+#0 D:\xampp\htdocs\kohana\application\views\site\form.php(2): Kohana_Core::error_handler(8, 'Undefined varia...', 'D:\xampp\htdocs...', 2, Array)
+#1 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(61): include('D:\xampp\htdocs...')
+#2 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(343): Kohana_View::capture('D:\xampp\htdocs...', Array)
+#3 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(228): Kohana_View->render()
+#4 D:\xampp\htdocs\kohana\application\views\template.php(22): Kohana_View->__toString()
+#5 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(61): include('D:\xampp\htdocs...')
+#6 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(343): Kohana_View::capture('D:\xampp\htdocs...', Array)
+#7 D:\xampp\htdocs\kohana\system\classes\kohana\controller\template.php(44): Kohana_View->render()
+#8 [internal function]: Kohana_Controller_Template->after()
+#9 D:\xampp\htdocs\kohana\system\classes\kohana\request\client\internal.php(121): ReflectionMethod->invoke(Object(Controller_Site))
+#10 D:\xampp\htdocs\kohana\system\classes\kohana\request\client.php(64): Kohana_Request_Client_Internal->execute_request(Object(Request))
+#11 D:\xampp\htdocs\kohana\system\classes\kohana\request.php(1138): Kohana_Request_Client->execute(Object(Request))
+#12 D:\xampp\htdocs\kohana\index.php(109): Kohana_Request->execute()
+#13 {main}
+2012-07-05 15:40:58 --- ERROR: ErrorException [ 8 ]: Undefined variable: article ~ APPPATH\views\site\form.php [ 2 ]
+2012-07-05 15:40:58 --- STRACE: ErrorException [ 8 ]: Undefined variable: article ~ APPPATH\views\site\form.php [ 2 ]
+--
+#0 D:\xampp\htdocs\kohana\application\views\site\form.php(2): Kohana_Core::error_handler(8, 'Undefined varia...', 'D:\xampp\htdocs...', 2, Array)
+#1 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(61): include('D:\xampp\htdocs...')
+#2 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(343): Kohana_View::capture('D:\xampp\htdocs...', Array)
+#3 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(228): Kohana_View->render()
+#4 D:\xampp\htdocs\kohana\application\views\template.php(22): Kohana_View->__toString()
+#5 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(61): include('D:\xampp\htdocs...')
+#6 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(343): Kohana_View::capture('D:\xampp\htdocs...', Array)
+#7 D:\xampp\htdocs\kohana\system\classes\kohana\controller\template.php(44): Kohana_View->render()
+#8 [internal function]: Kohana_Controller_Template->after()
+#9 D:\xampp\htdocs\kohana\system\classes\kohana\request\client\internal.php(121): ReflectionMethod->invoke(Object(Controller_Site))
+#10 D:\xampp\htdocs\kohana\system\classes\kohana\request\client.php(64): Kohana_Request_Client_Internal->execute_request(Object(Request))
+#11 D:\xampp\htdocs\kohana\system\classes\kohana\request.php(1138): Kohana_Request_Client->execute(Object(Request))
+#12 D:\xampp\htdocs\kohana\index.php(109): Kohana_Request->execute()
+#13 {main}
+2012-07-05 15:46:14 --- ERROR: ErrorException [ 8 ]: Undefined variable: article ~ APPPATH\views\site\form.php [ 2 ]
+2012-07-05 15:46:14 --- STRACE: ErrorException [ 8 ]: Undefined variable: article ~ APPPATH\views\site\form.php [ 2 ]
+--
+#0 D:\xampp\htdocs\kohana\application\views\site\form.php(2): Kohana_Core::error_handler(8, 'Undefined varia...', 'D:\xampp\htdocs...', 2, Array)
+#1 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(61): include('D:\xampp\htdocs...')
+#2 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(343): Kohana_View::capture('D:\xampp\htdocs...', Array)
+#3 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(228): Kohana_View->render()
+#4 D:\xampp\htdocs\kohana\application\views\template.php(22): Kohana_View->__toString()
+#5 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(61): include('D:\xampp\htdocs...')
+#6 D:\xampp\htdocs\kohana\system\classes\kohana\view.php(343): Kohana_View::capture('D:\xampp\htdocs...', Array)
+#7 D:\xampp\htdocs\kohana\system\classes\kohana\controller\template.php(44): Kohana_View->render()
+#8 [internal function]: Kohana_Controller_Template->after()
+#9 D:\xampp\htdocs\kohana\system\classes\kohana\request\client\internal.php(121): ReflectionMethod->invoke(Object(Controller_Site))
+#10 D:\xampp\htdocs\kohana\system\classes\kohana\request\client.php(64): Kohana_Request_Client_Internal->execute_request(Object(Request))
+#11 D:\xampp\htdocs\kohana\system\classes\kohana\request.php(1138): Kohana_Request_Client->execute(Object(Request))
+#12 D:\xampp\htdocs\kohana\index.php(109): Kohana_Request->execute()
+#13 {main}

ec65abaab503a4edf3a085a874a6b314c8e96c41
gilcierweb <gilcierweb@846f405d-fe61-700b-bed7-bc12fcc5fb76>
2012-07-09 19:46:55 +0000

    alterações

---
+++ trunk/kohana/application/logs/2012/07/09.php
@@ -0,0 +1,27 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2012-07-09 09:45:35 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: adm/banners/ddd/ddd ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2012-07-09 09:45:35 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: adm/banners/ddd/ddd ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 D:\xampp\htdocs\kohana\index.php(109): Kohana_Request->execute()
+#1 {main}
+2012-07-09 09:45:47 --- ERROR: ErrorException [ 1 ]: Class 'Model_Banner' not found ~ MODPATH\orm\classes\kohana\orm.php [ 37 ]
+2012-07-09 09:45:47 --- STRACE: ErrorException [ 1 ]: Class 'Model_Banner' not found ~ MODPATH\orm\classes\kohana\orm.php [ 37 ]
+--
+#0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main}
+2012-07-09 09:47:27 --- ERROR: ErrorException [ 1 ]: Class 'Model_Banner' not found ~ MODPATH\orm\classes\kohana\orm.php [ 37 ]
+2012-07-09 09:47:27 --- STRACE: ErrorException [ 1 ]: Class 'Model_Banner' not found ~ MODPATH\orm\classes\kohana\orm.php [ 37 ]
+--
+#0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main}
+2012-07-09 09:47:28 --- ERROR: ErrorException [ 1 ]: Class 'Model_Banner' not found ~ MODPATH\orm\classes\kohana\orm.php [ 37 ]
+2012-07-09 09:47:28 --- STRACE: ErrorException [ 1 ]: Class 'Model_Banner' not found ~ MODPATH\orm\classes\kohana\orm.php [ 37 ]
+--
+#0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main}
+2012-07-09 09:47:28 --- ERROR: ErrorException [ 1 ]: Class 'Model_Banner' not found ~ MODPATH\orm\classes\kohana\orm.php [ 37 ]
+2012-07-09 09:47:28 --- STRACE: ErrorException [ 1 ]: Class 'Model_Banner' not found ~ MODPATH\orm\classes\kohana\orm.php [ 37 ]
+--
+#0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main}
